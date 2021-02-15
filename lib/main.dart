8bbda68b147f56ffc52ab09b8029588909531714
FlutLab.io <>
2021-02-15 22:56:46 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Netflix Clone',
+        theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.black, accentColor: Colors.white),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+                body: TabBarVeiw(physics: NeverScrollableScrollPhysics(), children: <Widget>[
+              Container(),
+              Container(),
+              Container(),
+              Container()
+            ],
+            ),
+            bottomNavigationBar: BottomBar(),
+            )));
+  }
+}
